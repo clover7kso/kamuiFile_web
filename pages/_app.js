e45ebcd7bc111617b71ee840362467f07c595a08
Woony <clover7kso@naver.com>
2022-05-15 12:33:13 +0900

    feat: Add GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { CacheProvider } from "@emotion/react";
 import PropTypes from "prop-types";
 import { ThemeProvider, CssBaseline } from "@mui/material";
-
 import createEmotionCache from "../util/createEmotionCache";
 import lightTheme from "../styles/theme/lightTheme";
 import "../styles/global.css";
+import { useRouter } from "next/router";
+import { pageview } from "../util/pageview";
 
 const clientSideEmotionCache = createEmotionCache();
 
 const MyApp = (props) => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <CacheProvider value={emotionCache}>
--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,22 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
           <link rel="stylesheet" href="/fonts/fonts.css" />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
         </Head>
         <body>
           <Main />
