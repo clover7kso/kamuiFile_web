f1ef8b54f1ff2404eed818cbe5bd84b025342354
Woony <clover7kso@naver.com>
2022-06-20 22:55:02 +0900

    feat: ogtag test by title

--- util/seo.js
+++ util/seo.js
@@ -1,25 +1,30 @@
-export const seoDefault = {
-  title: "Kamui File - Transfer your files",
-  description: "Fast and Safe File Transfer",
-  canonical: "https://kamuifile.com/",
-  openGraph: {
-    type: "website",
-    url: "https://kamuifile.com/",
-    title: "Kamui File - Transfer your files",
-    description: "Fast and Safe P2P file transfer",
-    site_name: "File Transfer",
-    images: [
-      {
-        url: "https://kamuifile.com/ogtag.png",
-        width: 1200,
-        height: 630,
-        alt: "Kamui File",
-      },
-    ],
-  },
-  twitter: {
-    handle: "@handle",
-    site: "@site",
-    cardType: "summary_large_image",
-  },
+import useTranslation from "next-translate/useTranslation";
+
+export const seoDefault = () => {
+  const { t } = useTranslation("");
+  return {
+    title: t("common:og:title"),
+    description: "Fast and Safe File Transfer",
+    canonical: "https://kamuifile.com/",
+    openGraph: {
+      type: "website",
+      url: "https://kamuifile.com/",
+      title: "Kamui File - Transfer your files",
+      description: "Fast and Safe P2P file transfer",
+      site_name: "File Transfer",
+      images: [
+        {
+          url: "https://kamuifile.com/ogtag.png",
+          width: 1200,
+          height: 630,
+          alt: "Kamui File",
+        },
+      ],
+    },
+    twitter: {
+      handle: "@handle",
+      site: "@site",
+      cardType: "summary_large_image",
+    },
+  };
 };
