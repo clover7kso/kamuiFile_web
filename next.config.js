08d382c42554641a12b6554de1ff4af244046ea0
Woony <clover7kso@naver.com>
2022-07-01 15:53:04 +0900

    feat: fix bug

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ const { i18n } = require("./next-i18next.config");
 
 module.exports = {
   i18n,
+  webpack: (config) => {
+    // 아래를 추가합니다.
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
